4b8e3f16246924da8f8ad5063b0577ea086bae47
Eryn <erynli2016@gmail.com>
2015-11-24 10:30:57 -0600

    Update server.js

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.post('/update', function(req, res) {
         // watch for any connect issues
         if (err) console.log(err);
         conn.query(
-            'UPDATE salesforce.Contact SET Email = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
-            [req.body.newEmail.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.oldEmail.trim()],
+            'UPDATE salesforce.Contact SET Phone = $1, MobilePhone = $1 WHERE LOWER(FirstName) = LOWER($2) AND LOWER(LastName) = LOWER($3) AND LOWER(Email) = LOWER($4)',
+            [req.body.phone.trim(), req.body.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
             function(err, result) {
                 if (err != null || result.rowCount == 0) {
-                  conn.query('INSERT INTO salesforce.Contact (FirstName, LastName, Email) VALUES ($1, $2, $3)',
-                  [req.body.firstName.trim(), req.body.lastName.trim(), req.body.newEmail.trim()],
+                    alert($1);
+                  conn.query('INSERT INTO salesforce.Contact (Phone, FirstName, LastName, Email) VALUES ($1, $2, $3, $4, $5)',
+                  [req.body.phone.trim(), req.firstName.trim(), req.body.lastName.trim(), req.body.email.trim()],
                   function(err, result) {
                     done();
                     if (err) {
